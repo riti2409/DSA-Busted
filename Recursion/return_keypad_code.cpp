429ea60ff0b4386a84eb7e3c7e4e26ec59bd9787
Riti Kumari <ritikumariupadhyay24@gmail.com>
2022-02-01 23:20:18 +0530

    Update return_keypad_code.cpp

--- Recursion/return_keypad_code.cpp
+++ Recursion/return_keypad_code.cpp
@@ -1,43 +1,10 @@
-/*Given an integer n, using phone keypad find out all the possible strings that can be made using digits of input n.
-Return empty string for numbers 0 and 1.
-Note : 1. The order of strings are not important.
-2. Input and output has already been managed for you. You just have to populate the output array and return the count of elements populated in the output array.
-Input Format :
-
-Integer n
-
-Output Format :
-
-All possible strings in different lines
-
-Constraints :
-1 <= n <= 10^6
-Sample Input:
-
-23
-
-Sample Output:
-
-ad
-ae
-af
-bd
-be
-bf
-cd
-ce
-cf
-*/
-
 
 
 #include <string>
 using namespace std;
 
 int keypad(int num, string output[]){
-    /* Insert all the possible combinations of the integer number into the output string array. You do not need to
-    print anything, just return the number of strings inserted into the array.
-    */
+    
     string input;
     if(num == 0){
         output[0] = "";
