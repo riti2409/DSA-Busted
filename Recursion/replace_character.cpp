82e35dfdca59f7bb863dcd4cf08fd8b8faaca7de
Riti Kumari <ritikumariupadhyay24@gmail.com>
2022-02-01 23:19:53 +0530

    Update replace_character.cpp

--- Recursion/replace_character.cpp
+++ Recursion/replace_character.cpp
@@ -1,12 +1,7 @@
 
 
 void replaceCharacter(char input[], char c1, char c2) {
-    /* Don't write main().
-     * Don't read input, it is passed as function argument.
-     * No need to print or return the output. 
-     * Change in the given input string itself.
-     * Taking input and printing output is handled automatically.
-     */
+  
     if(input[0] == '\0')
         return;
     if(input[0] == c1)
