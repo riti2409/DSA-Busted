8194411555d978bcf9bd66f9e0da5e502a94a493
Riti Kumari <ritikumariupadhyay24@gmail.com>
2022-02-02 13:54:36 +0530

    Add files via upload

---
+++ Dynamic Programming/subset_sum_problem.cpp
@@ -0,0 +1,28 @@
+class Solution{   
+public:
+    bool isSubsetSum(int N, int arr[], int sum){
+        // code here
+        bool dp[N+1][sum+1];
+        for(int i=0;i<N+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0){
+                    dp[i][j]=false;
+                }
+                if(j==0){
+                    dp[i][j]=true;
+                }
+            }
+        }
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[N][sum];
+    }
+};
