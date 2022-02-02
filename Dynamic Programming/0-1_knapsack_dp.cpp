bb8ed732cf2fe807c2c0cc579d44e3be08f08d55
Riti Kumari <ritikumariupadhyay24@gmail.com>
2022-02-02 13:53:48 +0530

    Add files via upload

---
+++ Dynamic Programming/0-1_knapsack_dp.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       int dp[n+1][W+1];
+       for(int i=0;i<n+1;i++){
+           for(int j=0;j<W+1;j++){
+               if(i==0||j==0)
+               dp[i][j]=0;
+           }
+       }
+       for(int i=1;i<n+1;i++){
+           for(int j=1;j<W+1;j++){
+               if(wt[i-1]<=j){
+                   dp[i][j]= max((val[i-1]+dp[i-1][j-wt[i-1]]),dp[i-1][j]);
+               }
+               else{
+                   dp[i][j]=dp[i-1][j];
+               }
+           }
+       }
+       return dp[n][W];
+    }
+};
